39c48cb226fe6105b167b634d5dacb0c464b4771
Elyorwebdev <lutsiferdoom@gmail.com>
2023-02-04 12:36:19 +0500

    updated

---
+++ src/components/loader/loader.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./loader.css"
+
+const loader = () => {
+    return (
+        <div className='loader'>
+            <div className="container1">
+                <div className="wheel"></div>
+                <div className="wheel"></div>
+
+            </div>
+            <h3>Loading...</h3>
+        </div>
+
+
+
+    )
+}
+
+export default loader
